a62c96924df4c2e6e3c47d63d82674327ece3e35
mgonnet <mgonnet3@gmail.com>
2017-07-13 21:37:39 -0300

    Movimientos de piezas en archivo independiente

--- matias.hs
+++ matias.hs
@@ -1,4 +1,5 @@
 import Shogii
+import MovimientosPiezas
 
 showPieza :: Pieza -> String
 showPieza Rey = "R"
@@ -70,18 +71,6 @@ todoBienactionsDeUnaPieza = and casosactionsDeUnaPieza
 obtenerFuncionMovimientoDePieza :: Pieza -> ((Pieza,Coordenada,ShogiPlayer) -> [[Coordenada]])
 obtenerFuncionMovimientoDePieza Peon = movimientosPosiblesPeon 
 
-movimientosPosiblesPeon :: (Pieza, Coordenada, ShogiPlayer) -> [[Coordenada]]
-movimientosPosiblesPeon (a, (Coordenada x y), Sente) = if y==9 then [[]] else [[(Coordenada x (y+1))]]
-movimientosPosiblesPeon (a, (Coordenada x y), Gote) = if y==1 then [[]] else [[(Coordenada x (y-1))]]
-
-caso1MovimientosPosiblesPeon = (movimientosPosiblesPeon (Peon, (Coordenada 1 1), Sente)) == [[(Coordenada 1 2)]]
-caso2MovimientosPosiblesPeon = (movimientosPosiblesPeon (Peon, (Coordenada 1 9), Gote)) == [[(Coordenada 1 8)]]
-caso3MovimientosPosiblesPeon = (movimientosPosiblesPeon (Peon, (Coordenada 1 9), Sente)) == [[]]
-caso4MovimientosPosiblesPeon = (movimientosPosiblesPeon (Peon, (Coordenada 1 1), Gote)) == [[]]
-
-casosMovimientosPosiblesPeon = caso1MovimientosPosiblesPeon:caso2MovimientosPosiblesPeon:caso3MovimientosPosiblesPeon:caso4MovimientosPosiblesPeon:[]
-
-todoBienMovimientosPosiblesPeon = and casosMovimientosPosiblesPeon
 
 
 puedePromover :: ShogiAction -> Pieza -> ShogiPlayer -> Bool
